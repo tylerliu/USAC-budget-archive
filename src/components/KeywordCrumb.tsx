3aa6fd80378954cfa758ed298c972d7a9de2d914
tylerliu <tylersiqi@163.com>
2020-08-16 22:14:42 -0700

    clean up

--- src/components/KeywordCrumb.tsx
+++ src/components/KeywordCrumb.tsx
@@ -16,21 +16,23 @@ export default class KeywordCrumb extends Component<DataLoaderProps> {
     render() {
         let list = this.props.dataloader.getFilters()
         return (
-            <Breadcrumbs separator=">" style={this.props.style}>
-                <Typography>Fliters Applied:</Typography>
-                <Tooltip title="Remove All Filters">
-                    <Link key={-1} color="textPrimary"
-                        onClick={() => this.props.dataloader.sliceFilter(0)}>Expenses [${KMFormat(this.props.dataloader.getDatasetTotal())}]</Link>
-                </Tooltip>
-                {list.slice(0, -1).map((filter, index) => (
-                    <Tooltip title="View This Filter">
-                        <Link key={index} color="textSecondary"
-                              onClick={() => this.props.dataloader.sliceFilter(index + 1)}>{filter.category}: {filter.name} [${KMFormat(filter.amount)}]</Link>
-                    </Tooltip>
-                ))}
-                {list.length > 0 ? (<Typography color="textPrimary"
-                                                key={list.length}>{list[list.length - 1].category}: {list[list.length - 1].name} [${KMFormat(list[list.length - 1].amount)}]</Typography>) : null}
-            </Breadcrumbs>
+                <Breadcrumbs separator=">" style={this.props.style}>
+                    <div style={{display: "flex"}}>
+                        <Typography>Fliters Applied:&nbsp;&nbsp;</Typography>
+                        <Tooltip title="Remove All Filters">
+                            <Link key={-1} color="textPrimary"
+                                  onClick={() => this.props.dataloader.sliceFilter(0)}>Expenses [${KMFormat(this.props.dataloader.getDatasetTotal())}]</Link>
+                        </Tooltip>
+                    </div>
+                    {list.slice(0, -1).map((filter, index) => (
+                        <Tooltip title="View This Filter">
+                            <Link key={index} color="textSecondary"
+                                  onClick={() => this.props.dataloader.sliceFilter(index + 1)}>{filter.category}: {filter.name} [${KMFormat(filter.amount)}]</Link>
+                        </Tooltip>
+                    ))}
+                    {list.length > 0 ? (<Typography color="textPrimary"
+                                                    key={list.length}>{list[list.length - 1].category}: {list[list.length - 1].name} [${KMFormat(list[list.length - 1].amount)}]</Typography>) : null}
+                </Breadcrumbs>
         )
     }
 }
